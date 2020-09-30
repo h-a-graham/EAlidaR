402f973f2d4be10fe7e30f7bd4ac504268b3b453
Graham <hg340@exeter.ac.uk>
2020-09-30 14:17:49 +0100

    minor tests etc trying to get running wiht R selenium but need to use personal laptop...

--- Working/scrape_arc_ids.R
+++ Working/scrape_arc_ids.R
@@ -1,23 +1,57 @@
 library(tidyverse)
 library(rvest)
-library(splashr)
-library(stevedore)
-library(reticulate)
+# library(splashr)
+# library(stevedore)
+# library(reticulate)
+# library(wdman)
+
 
 library(RSelenium)
-library(wdman)
+rD <- RSelenium::rsDriver(
+  port = 4444L,
+  browser = c("firefox"),
+  version = "latest"
+)
 
-selServ <- selenium(verbose = FALSE)
-selServ$process
+driver <- RSelenium::rsDriver(browser = "chrome",
+                              chromever =
+                                system2(command = "wmic",
+                                        args = 'datafile where name="C:\\\\Program Files (x86)\\\\Google\\\\Chrome\\\\Application\\\\chrome.exe" get Version /value',
+                                        stdout = TRUE,
+                                        stderr = TRUE) %>%
+                                stringr::str_extract(pattern = "(?<=Version=)\\d+\\.\\d+\\.\\d+\\.") %>%
+                                magrittr::extract(!is.na(.)) %>%
+                                stringr::str_replace_all(pattern = "\\.",
+                                                         replacement = "\\\\.") %>%
+                                paste0("^",  .) %>%
+                                stringr::str_subset(string =
+                                                      binman::list_versions(appname = "chromedriver") %>%
+                                                      dplyr::last()) %>%
+                                as.numeric_version() %>%
+                                max() %>%
+                                as.character())
 
-rD <- rsDriver()
+remote_driver <- driver[["client"]]
 
 
-use_condaenv(condaenv = "reticulate_env", conda = file.path('C:/Users/hg340/AppData/Local/Continuum/miniconda3'))
-install_splash()
-splash_container <- start_splash()
 
 
+
+driver <- rsDriver(browser=c("chrome"))
+remote_driver <- driver[["client"]]
+remote_driver$open()
+
+
+
+link <- 'https://environment.data.gov.uk/DefraDataDownload/?Mode=survey'
+
+
+
+# use_condaenv(condaenv = "reticulate_env", conda = file.path('C:/Users/hg340/AppData/Local/Continuum/miniconda3'))
+# install_splash()
+# splash_container <- start_splash()
+#
+#
 
 
 
