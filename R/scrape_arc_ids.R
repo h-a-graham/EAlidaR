ebe73bd558ea5b66aaef083842bde38da6824f89
h-a-graham <hg340@exeter.ack.uk>
2020-10-07 18:39:07 +0100

    bringing things up to date - lots of reorganisation - think it requires rethink with the search areas though... Mainly adding the python function as a seperate script and sourcing it with reticulate. let's consider using EA coverage tiles to extract instead of OS tiles?

---
+++ R/scrape_arc_ids.R
@@ -0,0 +1,82 @@
+
+#' @importFrom magrittr %>%
+
+#' @export
+scrape_tile_IDs <- function(conda_path, env_name, gecko_exe){
+  options(warn=1)
+
+  warning("This function shouldn't be necessary aslo it takes a long time, will prevent the use of your machine during the run,
+  requires reticulate and an associated conda environment with the following packages installed: selenium, pyautogui, pandas")
+
+  question1 <- readline("Are you sure you want to run this? (Y/N)")
+
+  if(regexpr(question1, 'y', ignore.case = TRUE) == 1){
+    print("Running 'scrape_tile_IDs' - this may take a while...")
+  }else{
+    print("Execution of 'scrape_tile_IDs' cancelled")
+    return()
+  }
+
+
+  #set up python...
+  reticulate::use_condaenv(condaenv =conda_path, env_name, required = TRUE)
+
+  # Source python functions...
+  reticulate::source_python("python/EA_scrape_functions.py")
+  py_out <- scrapestuff(gecko_exe, work_dir=getwd())
+
+  # organise outputs from python as list
+  py_returns <- list(arc_ids= py_out[[1]], error_log = py_out[[2]])
+
+  return(py_returns)
+
+}
+
+
+
+#' @export
+check_tiles <- function(.scrape_out){
+
+  grid_sf <- sf::read_sf('data/OSGB_ENG_Grid_25km_.gpkg') %>%
+    tibble::rownames_to_column(var = "grid_id") %>%
+    dplyr::mutate(grid_id = as.numeric(grid_id))%>%
+    dplyr::left_join(., .scrape_out$arc_ids, by = c("grid_id"= "tile_n")) %>%
+    dplyr::mutate(Retrieved = ifelse(is.na(arc_code), "False", "True"))%>%
+    dplyr::mutate(Retrieved = forcats::fct_relevel(Retrieved, 'True'))
+
+
+  t_plot <- ggplot2::ggplot() +
+    # loads background map tiles from a tile source - rosm::osm.types() for osm options
+    ggspatial::annotation_map_tile(type = "cartolight", zoomin = -1, ) +
+
+    # raster layers train scales and get projected automatically
+    ggspatial::layer_spatial(grid_sf, ggplot2::aes(fill = Retrieved), alpha = 0.5)+
+
+    ggplot2::scale_fill_manual(values=c("#49F585", "#BAC0BC")) +
+
+    ggplot2::coord_sf(crs = 27700, datum = sf::st_crs(27700))
+
+  missing_tiles <- as.data.frame(grid_sf) %>%
+    dplyr::filter(is.na(arc_code)) %>%
+    dplyr::select(-c(geom, arc_code))
+
+  return(list(tile_plot = t_plot, missing_tile_df = missing_tiles))
+
+}
+
+
+
+#' @export
+save_arc_IDs <- function(.dataframe){
+# convert the pandas DF object to R DF and save as RDS
+r_df <- reticulate::py$arc_id_df
+save_path <- file.path(wd, 'data', 'arc_ids_25km.rds')
+saveRDS(r_df, file = save_path)
+return(readRDS(save_path))
+}
+
+
+
+
+
+
