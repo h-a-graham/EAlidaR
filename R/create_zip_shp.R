7af5565fc7a3fe52d2a8064658d6cb3eb350846b
hg340 <hg340@exeter.ac.uk>
2020-10-09 13:31:52 +0100

    Think scraping is finally working - now the challange of retrieving  ~ 6000 codes...

--- R/create_zip_shp.R
+++ R/create_zip_shp.R
@@ -8,14 +8,14 @@ zip_shp <- function(.val, .path, .sfObj){
   feature <- .val %>%
     sf::st_sfc()%>%
     sf::st_set_crs(sf::st_crs(.sfObj)) %>%
-    sf::st_buffer(-1000)
+    sf::st_buffer(-500)
 
   cell.name <- feature %>%
     sf::st_intersection(.sfObj, .) %>%
     dplyr::pull(grid_id)
 
   sf::write_sf(feature, file.path(.path, stringr::str_c(cell.name, '.shp')))
-  filelist = Sys.glob(file.path(.path, stringr::str_c(cell.name, '*')))
+  filelist <- Sys.glob(file.path(.path, stringr::str_c(cell.name, '*')))
   zip::zipr(file.path(.path, stringr::str_c('Tile_',cell.name, '.zip')), files = filelist)
   purrr::map(filelist, file.remove)
 }
@@ -25,14 +25,13 @@ create_zip_tiles <- function(){
 direc <- file.path('data/grid_shp_zip')
 dir.create(direc, showWarnings = FALSE)
 
-grid_25km.path <- 'data/OSGB_Grid_5km_id.gpkg'
+grid_5km.path <- 'data/5km_Grid_LiDAR_inter.gpkg'
 
-grid_25km <- sf::read_sf(grid_25km.path) %>%
+grid_5km <- sf::read_sf(grid_5km.path) %>%
   dplyr::select(grid_id, geom)
 
-
-sf::st_geometry(grid_25km)%>%
-  purrr::map(~ zip_shp(.val = ., .path = direc, .sfObj = grid_25km))
+sf::st_geometry(grid_5km)%>%
+  purrr::map(~ zip_shp(.val = ., .path = direc, .sfObj = grid_5km))
 
 }
 
