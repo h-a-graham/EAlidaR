18a776114c53f74dea2c27278cdbd4bcb21ab225
hg340 <hg340@exeter.ac.uk>
2020-10-13 19:08:45 +0100

    Now almost all functions working as desired - fix issue with saving multiple files in get_area

--- R/get_lidar.R
+++ R/get_lidar.R
@@ -1,19 +1,44 @@
 
-compose_url <- function(res, os.tile){
+
+get_arc_id <- function(tile.string){
+  req_tile <- stringr::str_to_upper(tile.string)
+
+  tiles_10km <- readRDS('data/coverage_10km_sf.rds')
+
+  tiles_5km <- readRDS('data/tile_within10km.rds')
+
+  sf::st_agr(tiles_10km) = "constant"
+  sf::st_agr(tiles_5km) = "constant"
+
+  tile_id <- tiles_5km %>%
+    dplyr::filter(TILE_NAME == req_tile) %>%
+    sf::st_buffer(., -100)%>%
+    sf::st_intersection(tiles_10km) %>%
+    dplyr::pull(arc_code)
+
+  return(tile_id)
+
+}
+
+
+compose_url <- function(res, os.tile, mod.type){
 
   if (res == 1 || res == 2){
     res.str <- sprintf('%sM',res)
-  } else if (res == 0.25 || res == 0.5){
-    res.str <- sprintf('%sCM',res)
+  } else if (res == 0.25 || res == res_cm){
+    res_cm <- res * 100
+    res.str <- sprintf('%sCM',res_cm)
   } else {
     stop('The resolution requested is not available options are: 0.25, 0.5, 1 and 2')
   }
 
-  sprintf('https://environment.data.gov.uk/UserDownloads/interactive/ded85a5670bb4c80bbe5e300ba292c6a25520/LIDARCOMP/LIDAR-DSM-%s-%s.zip', res.str, os.tile)
+  arc_web_id <- get_arc_id(os.tile)
 
-          # 'https://environment.data.gov.uk/UserDownloads/interactive/ded85a5670bb4c80bbe5e300ba292c6a25520/LIDARCOMP/LIDAR-DTM-2m-2019-SX69se.zip'
-          # 'https://environment.data.gov.uk/UserDownloads/interactive/44d4901e024a4dc18e3c448d3544ea1a98736/LIDARCOMP/LIDAR-DSM-2M-SX69ne.zip'
-  # 'https://environment.data.gov.uk/UserDownloads/interactive/e42cd3cb53754adbb7a6b74e8b783a4953340/LIDARCOMP/LIDAR-DTM-2m-2019-SX69se.zip
+  if (mod.type == 'DTM'){
+    download_url <- sprintf('https://environment.data.gov.uk/UserDownloads/interactive/%s/LIDARCOMP/LIDAR-DTM-%s-2019-%s.zip', arc_web_id, res.str, os.tile)
+  } else if (mod.type == 'DSM') {
+    download_url <- sprintf('https://environment.data.gov.uk/UserDownloads/interactive/%s/LIDARCOMP/LIDAR-DSM-%s-%s.zip', arc_web_id, res.str, os.tile)
+  }
 
 }
 
@@ -36,62 +61,204 @@ join_paths <- function(p1, p2){
 #' @export
 merge_ostiles <- function(ras.folder){
   ras.list <- list.files(ras.folder)
+  ras.list <- purrr::discard(ras.list , grepl(".tif.xml|.tfw|index", ras.list ))
+
   ras.list <- lapply(ras.list, join_paths, p2=ras.folder)
-  ras.list <- lapply(ras.list, read_raster)
-  ras.merge <- do.call(raster::merge, ras.list)
-  raster::crs(ras.merge)<- sp::CRS('+init=EPSG:27700')   #'+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +units=m +no_defs'
+
+  if (length(ras.list) > 1){
+    ras.list <- lapply(ras.list, read_raster)
+    ras.merge <- do.call(raster::merge, ras.list)
+  } else if(length(ras.list) == 1){
+
+    ras.merge <- raster::raster(file.path(ras.list[[1]]))
+
+  }
+
+  raster::crs(ras.merge)<- sp::CRS('+init=EPSG:27700')
+
+
   return(ras.merge)
 }
 
 
 #' @export
-get_tile <- function(resolution, os.tile.name, dest.folder, unzip.file, merge.tiles, save.tile, ras.format){
+get_tile <- function(os.tile.name, resolution, model.type, dest.folder, merge.tiles, ras.format){
+
+  TempRasDir <- tempdir()
+
+
+  if(!(model.type == 'DTM' || model.type == 'DSM')){
+    stop('Only DTM and DSM model types are supported at present.')
+  }
 
   if (missing(dest.folder)) {
     dest.folder <- tempdir()
-    message('No destination folder provided - saving to temp directory...')
-  }
-
-  if (missing(unzip.file)){
-    unzip.file <- TRUE
+    save.tile <- FALSE
+  } else {
+    save.tile <- TRUE
   }
 
   if (missing(merge.tiles)){
     merge.tiles <- TRUE
   }
 
-  if (merge.tiles == TRUE & unzip.file == FALSE){
-    warning('unzip.file arg set to false but mosaic.tiles set as TRUE. Setting unzip.file to TRUE by default')
-    unzip.file <- TRUE
-  }
-
-  if (missing(save.tile)){
-    save.tile <- FALSE
-  }
   if (missing(ras.format)){
     ras.format <- "GTiff"
   }
 
-  web.url <- compose_url(res=resolution, os.tile = os.tile.name)
-  dest.path <- compose_zip_path(save.folder = dest.folder, web.add = web.url)
-  download.file(url=web.url, destfile=dest.path, method='auto', quiet = FALSE)
-
-  if (unzip.file == TRUE){
-    exp.fold <- tools::file_path_sans_ext(dest.path)
-    zip::unzip(zipfile = dest.path, overwrite = TRUE, exdir = exp.fold)
-    unlink(dest.path, recursive = TRUE, force=TRUE)
-    dest.path <- exp.fold
+  rasformats <- raster::writeFormats()[,1]
+  if (!(ras.format %in% rasformats)){
+    stop('Requested Raster format not supported. Use raster::writeFormats() to view supported drivers')
   }
+
+  web.url <- compose_url(res=resolution, os.tile = os.tile.name, mod.type = model.type)
+  dest.path <- compose_zip_path(save.folder = TempRasDir, web.add = web.url)
+
+  tryCatch({
+  download.file(url=web.url, destfile=dest.path, method='auto', quiet = FALSE)
+  },
+  error=function(cond) {
+    message("Requested tile is not available!!! \n
+            Either: (1) No data is availale in this tile or \n
+            (2) try a different resolution... \n ")
+    # message("Original error message:")
+    # message(cond)
+    # Choose a return value in case of error
+    return()
+  })
+
+
+  exp.fold <- tools::file_path_sans_ext(dest.path)
+  zip::unzip(zipfile = dest.path, overwrite = TRUE, exdir = exp.fold)
+  unlink(dest.path, recursive = TRUE, force=TRUE)
+  dest.path <- exp.fold
+
 
   if (merge.tiles == TRUE){
     ras.obj <- merge_ostiles(dest.path)
     if (save.tile == TRUE){
-      raster::writeRaster(ras.obj, file.path(dest.folder, os.tile.name), format=ras.format, overwrite=TRUE, options = c("COMPRESS=LZW"))
-
+      ras.obj <- raster::writeRaster(ras.obj, file.path(dest.folder, os.tile.name), format=ras.format, overwrite=TRUE, options = c("COMPRESS=LZW"))
+      unlink(dest.path, recursive = TRUE, force=TRUE)
     }
-    unlink(dest.path, recursive = TRUE, force=TRUE)
+
     return(ras.obj)
   }
 
   return(dest.path)
 }
+
+
+resave_rasters <- function(ras, folder, ras_format){
+  print('B')
+  save_name <- tools::file_path_sans_ext(basename(ras[[2]]@file@name))
+  print(save_name)
+  out_ras <- raster::writeRaster(ras, file.path(folder, save_name), format=ras_format, overwrite=TRUE, options = c("COMPRESS=LZW"))
+  return(out_ras)
+}
+
+
+#' @export
+get_area <- function(poly_area, resolution, model.type, merge.tiles, crop, dest.folder, out.name, ras.format){
+
+  if (merge.tiles == TRUE & !missing(dest.folder) & missing(out.name)){
+    stop('When saving a merged raster (merged.tiles = TRUE) you must also provide a name (i.e out.name = "MyArea)"')
+  }
+
+  if(!(model.type == 'DTM' || model.type == 'DSM')){
+    stop('Only "DTM" and "DSM" model types are supported at present.')
+  }
+
+  if (class(poly_area)[1] == "sf"){
+    sf_geom <- poly_area
+  } else {
+    sf_geom <- sf::read_sf(poly_area)
+  }
+
+  if (sf::st_crs(sf_geom)$epsg != 27700){
+    sf_geom <- sf_geom %>%
+      sf::st_transform(27700)
+  }
+
+
+  if (missing(dest.folder)) {
+    save.tile <- FALSE
+    dest.folder <- tempdir()
+    message('No destination folder provided - saving to temp directory...')
+  }else {
+    save.tile <- TRUE
+  }
+
+  if (missing(merge.tiles)){
+    merge.tiles <- TRUE
+  }
+
+  if (missing(crop)){
+    crop <- FALSE
+  }
+
+  if (crop == TRUE & merge.tiles == FALSE){
+    crop==FALSE
+    message(' "crop" arg. ignored - crop only applies when "merge.tiles" is TRUE')
+  }
+
+  if (missing(ras.format)){
+    ras.format <- "GTiff"
+  }
+
+  tiles_5km <- readRDS('data/tile_within10km.rds')
+
+  sf::st_agr(sf_geom) = "constant"
+  sf::st_agr(tiles_5km) = "constant"
+
+  tile_5km_inter <- tiles_5km %>%
+    sf::st_intersection(sf_geom)%>%
+    dplyr::pull(TILE_NAME) %>%
+    gsub("([0-9]\\D+)", "\\L\\1",.,perl=TRUE)
+
+
+  collect_tiles_safe <- function(x) {
+    f = purrr::possibly(function() get_tile(os.tile.name = x, resolution = resolution, model.type = model.type), otherwise = NA_real_)
+    f()
+  }
+
+
+  ras_list <- tile_5km_inter %>%
+    purrr::map( ~ collect_tiles_safe(.))
+
+  # remove any NA values produced  by missing tiles
+  ras_list <- ras_list[!is.na(ras_list)]
+
+  if (merge.tiles ==TRUE){
+    if (length(ras_list) > 1){
+      ras.merge <- do.call(raster::merge, ras_list)
+    } else if(length(ras_list) == 1){
+
+      ras.merge <- ras_list[[1]]
+
+    }
+
+    if (crop==TRUE){
+      ras.merge <- raster::crop(ras.merge, sf_geom)
+    }
+
+    if (save.tile == TRUE){
+      ras.merge <- raster::writeRaster(ras.merge, file.path(dest.folder, out.name), format=ras.format, overwrite=TRUE, options = c("COMPRESS=LZW"))
+    }
+    return(ras.merge)
+  } else {
+    if (save.tile == TRUE){
+      print('B1')
+      ras_list <- ras_list %>%
+        purrr::map(~ resave_rasters(ras=., folder = dest.folder, ras_format = ras.format))
+      print('B2')
+      return(ras_list)
+    }
+    return(ras_list)
+  }
+
+
+
+
+  return(ras_list)
+}
+
--- tests/Run_test.R
+++ tests/Run_test.R
@@ -1,10 +1,7 @@
-# library(raster)
+
 # library(ggplot2)
 # library(ggspatial)
-# library(sf)
-# library(lwgeom)
-# library(tidyverse)
-# library(zip)
+
 devtools::document()
 # devtools::uninstall()
 devtools::load_all() # loads package functions
@@ -27,38 +24,58 @@ env.n <- 'R_python'
 # gecko.e <- 'C:/install_files/gecko/geckodriver-v0.27.0-win64/geckodriver.exe'
 gecko.e <- 'C:/Installation_Files/gecko/geckodriver-v0.27.0-win64/geckodriver.exe'
 
-py_out <- scrape_tile_IDs(conda_path = conda.p, env_name = env.n, gecko_exe = gecko.e)
+# py_out <- scrape_tile_IDs(conda_path = conda.p, env_name = env.n, gecko_exe = gecko.e)
 
-# Check results
-py_out$error_log
-py_out$arc_ids
-review_scrape <- check_tiles(py_out)
+# # Check results
+# py_out$error_log
+# py_out$arc_ids
+# review_scrape <- check_tiles(py_out)
 
-review_scrape$tile_plot
-review_scrape$missing_tile_df
+# review_scrape$tile_plot
+# review_scrape$missing_tile_df
+#
+# # run again to mop up where errors ahve occured - use 'previous' arg to include last output...
+# py_out2 <- scrape_tile_IDs(conda_path = conda.p, env_name = env.n, gecko_exe = gecko.e, previous=py_out)
+#
+# py_out3 <- scrape_tile_IDs(conda_path = conda.p, env_name = env.n, gecko_exe = gecko.e, previous=py_out2)
 
-py_out2 <- scrape_tile_IDs(conda_path = conda.p, env_name = env.n, gecko_exe = gecko.e, previous=py_out)
-
-py_out3 <- scrape_tile_IDs(conda_path = conda.p, env_name = env.n, gecko_exe = gecko.e, previous=py_out2)
-
-review_scrape <- check_tiles(py_out3)
-
-review_scrape$tile_plot
-save_arc_IDs(py_out3)
-
+# review_scrape <- check_tiles(py_out3)
+#
+# review_scrape$tile_plot
+# save_arc_IDs(py_out3)
+#
+#
+# sf_save_obj <- scrape_to_sf(py_out3)
 
 
-save_folder <- 'C:/HG_Projects/SideProjects/EA_Lidar_Check/EA_Download_TEST'
 
-rasOB <- get_tile(resolution = 2, os.tile.name = 'SX69se', dest.folder = save_folder, save.tile=TRUE)
+
+# cover_save <- 'C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/10km_coverage.gpkg'
+#
+# sf::write_sf(sf_save_obj$cover_sf, cover_save)
+#
+# km5inkm10 <- sf::read_sf('C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/5km_within10km.gpkg') %>%
+#   dplyr::select(TILE_NAME, geom) %>%
+#   saveRDS(., 'data/tile_within10km.rds')
+
+library(ggplot2)
+library(ggspatial)
+
+devtools::load_all()
+save_folder <- 'C:/HG_Projects/SideProjects/EALidarCheck/EADownloadTEST'
+
+rasOB <- get_tile(resolution = 2, model.type = 'DTM' , os.tile.name = 'SU66nw', dest.folder = save_folder)
+rasOB2 <- get_tile(resolution = 2, model.type = 'DSM' , os.tile.name = 'SK36ne')
+
+# out <- raster::writeRaster(rasOB, 'C:/HG_Projects/SideProjects/EALidarCheck/EADownloadTEST/testingagain', format = 'GTiff', overwrite=TRUE)
 
 
 ggplot() +
   # loads background map tiles from a tile source - rosm::osm.types() for osm options
-  annotation_map_tile(type = "osm", zoomin = -1) +
+  annotation_map_tile(type = "hillshade", zoomin = -1) +
 
   # raster layers train scales and get projected automatically
-  layer_spatial(rasOB, aes(colour = stat(band1)), alpha = 0.5) +
+  layer_spatial(rasAreaTest, alpha = 0.8) +
   # make no data values transparent
   scale_fill_viridis_c(na.value = NA) +
 
@@ -67,62 +84,40 @@ ggplot() +
 
   # spatial-aware automagic north arrow
   annotation_north_arrow(location = "br", which_north = "true") +
+  guides(fill=FALSE)+
   theme_bw() +
   labs(fill='Elevation (m)')
 
-ggsave('C:/HG_Projects/SideProjects/EA_Lidar_Check/maps/SX69se.jpg')
+
+
+devtools::load_all()
+save_folder <- 'C:/HG_Projects/SideProjects/EALidarCheck/EADownloadTEST/test2'
+poly_sf <- sf::read_sf('tests/Test_Area2.gpkg')
+
+rasAreaTest <- get_area(poly_area = poly_sf, resolution = 2, model.type = 'DTM', merge.tiles=FALSE, crop=FALSE, dest.folder = save_folder, out.name = 'TestArea') #, dest.folder, out.name, ras.format
 
 
 
+ggplot() +
+  # loads background map tiles from a tile source - rosm::osm.types() for osm options
+  annotation_map_tile(type = "hillshade", zoomin = -1) +
+
+  annotation_spatial(poly_sf, size = 2, col = "black") +
+  # raster layers train scales and get projected automatically
+  layer_spatial(rasAreaTest, alpha = 0.8) +
+  # make no data values transparent
+  scale_fill_viridis_c(na.value = NA) +
+
+  # spatial-aware automagic scale bar
+  annotation_scale(location = "tl") +
+
+  # spatial-aware automagic north arrow
+  annotation_north_arrow(location = "br", which_north = "true") +
+  guides(fill=FALSE)+
+  theme_bw() +
+  labs(fill='Elevation (m)')
 
 
 
-
-
-full_grid <- sf::read_sf('data/OSGB_Grid_5km.gpkg')
-
-
-plot(full_grid)
-
-st_geohash(full_grid, precision = 0)
-
-SX69se_grid <- full_grid %>%
-  filter(TILE_NAME == 'SX69SE')
-
-osgb_to_geohash <- function(sf.obj){
-  st_transform(sf.obj, crs = sp::CRS('+init=EPSG:4326'))%>%
-    st_geohash(., precision = 5)
-}
-
-geohash <- osgb_to_geohash(SX69se_grid)
-geohash
-
-bounds1 <- st_boundary(SX69se_grid)$geom
-bounds1[[1]]
-
-bounds2 <- st_boundary(st_transform(SX69se_grid, crs = sp::CRS('+init=EPSG:4326')))$geom
-
-sf::write_sf(SX69se_grid, 'C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/SX69se.shp')
-
-
-
-search_areas <- 'C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/OS50km_Lidar_Ext.gpkg'
-
-
-sa_gdf_shrink <- sf::read_sf(search_areas) %>%
-  st_buffer(., dist = -100) %>%
-  select(TILE_NAME, geom)
-
-plot(sa_gdf_shrink)
-
-
-TL_test <- sa_gdf_shrink %>%
-  filter(TILE_NAME == 'TLNE')
-
-plot(TL_test)
-
-sf::write_sf(TL_test, 'C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/TLNE.shp')
-filelist = Sys.glob('C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/TLNE.*')
-zip::zipr('C:/HG_Projects/SideProjects/EA_Lidar_Check/vectors/TL.zip', files = filelist)
-
+devtools::install_github("tylermorganwall/rayshader")
 
