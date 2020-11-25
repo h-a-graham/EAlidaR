60906054730cd0082abe8502a4aaaea41fc00a3d
hg340 <hg340@exeter.ac.uk>
2020-11-25 06:32:12 +0000

    moved scraping file to raw data

--- data-raw/Arc_ID_Scraping_Tests.R
+++ data-raw/Arc_ID_Scraping_Tests.R
@@ -4,7 +4,7 @@ devtools::load_all() # loads package functions
 #create the zipped files for uploading to the defra portal:
 grid_zip_folder<- system.file("extdata", "grid_shp_zip", package = "EAlidaR")
 
-create_zip_tiles(out.path = grid_zip_folder)
+# create_zip_tiles(out.path = grid_zip_folder)
 
 
 ## Scraping the required tile Arc Web Map IDs
